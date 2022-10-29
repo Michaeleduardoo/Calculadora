87ceef4869587598be2a2ad744f9236aa4c9b677
Michaeleduardoo <michaeledu@outlook.com>
2022-10-28 21:22:11 -0300

    Nova Calculadora

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+function adicionarCaracter(caracter) {
+    const valorInput = document.querySelector(".display").value
+    
+    document.querySelector(".display").value = valorInput + caracter
+
+}
+
+function limpaTela(){
+    document.querySelector(".display").value = "0"
+}
+
+function calcular(){
+    const valorInput = document.querySelector(".display").value
+
+    document.querySelector(".display").value = eval(valorInput)
+}
+
+function inverterNumero(){
+    const valorInput = document.querySelector(".display").value
+
+    document.querySelector(".display").value = valorInput * -1
+
+}
+
