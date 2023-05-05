2045cb40044993029a86acc0b53f45de5dc5bdb6
Michaeleduardoo <michaeledu@outlook.com>
2023-05-04 21:40:36 -0300

    Organização

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-
-
 function adicionarCaracter(caracter) {
     const valorInput = document.querySelector(".display").value
-    
+
     document.querySelector(".display").value = valorInput + caracter
 
 }
 
-function limpaTela(){
+function limpaTela() {
     document.querySelector(".display").value = "0"
 }
 
-function calcular(){
+function calcular() {
     const valorInput = document.querySelector(".display").value
 
     document.querySelector(".display").value = eval(valorInput)
 }
 
-function inverterNumero(){
+function inverterNumero() {
     const valorInput = document.querySelector(".display").value
 
     document.querySelector(".display").value = valorInput * -1
 
 }
-
